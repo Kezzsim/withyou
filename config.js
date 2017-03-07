cd1b4153b18f5eb101e1f0b25c7304c6e34fc37c
Kari A Barry <Kevin.A.Barry@stonybrook.edu>
2017-03-07 13:41:20 -0500

    Added Secret Keys for Twitter Login, now the app will just work when run

--- config.js
+++ config.js
@@ -7,8 +7,8 @@
  REMEMBER TO CLEAR THESE VALUES BEFORE PUSH TO GIT, UNTIL YOU GET THE REAL VALUES FROM OUR ACTUAL TWITTER ACCOUNT
  */
 module.exports = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: ''
+  consumer_key: 'jrqszscm2KXOXFoq5U49Wjc7m',
+  consumer_secret: 'cS376trOj6mlO4mpLdJYCsTEQLn7Uky7MzasVuqiQTXDDqRZpn',
+  access_token: '836314784932982784-YEyw3X3CGzZfe2T8QVybvmYUgEkOLoG',
+  access_token_secret: 'PwBoqYEkphvgW8Q9O6UhgL8tnLYKWKEMaE2NxL1nd5VuV'
 }
