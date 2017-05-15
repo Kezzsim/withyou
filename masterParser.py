b436dd138d869f8b611e31b71c8c1a6e387c59bf
Kari Barry <kevin.a.barry@stonybrook.edu>
2017-05-15 16:47:17 -0400

    Output file test (no redundant check)

--- masterParser.py
+++ masterParser.py
@@ -4,5 +4,7 @@ import re
 filePrefixes = ["imam", "priest", "Rabbi", "sufi", "blackreverend", "zenmonk"]
 for fi in filePrefixes:
  output = os.popen('node twitterbot.js ' + fi).read()
-
-print ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",output).split()) 
+ file = open(fi +"_output.txt","w")
+ file.write(' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",output).split()))
+ file.close
+#print ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",output).split())
