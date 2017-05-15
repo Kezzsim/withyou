19987bbb2b3edf57951e59dc77468e652d1581f9
Kari Barry <kevin.a.barry@stonybrook.edu>
2017-05-15 15:55:57 -0400

    Word cloud output for all personalities

--- masterParser.py
+++ masterParser.py
@@ -1,6 +1,8 @@
 import os
+import re
 
 filePrefixes = ["imam", "priest", "Rabbi", "sufi", "blackreverend", "zenmonk"]
 for fi in filePrefixes:
  output = os.popen('node twitterbot.js ' + fi).read()
-print output
+
+print ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",output).split()) 
